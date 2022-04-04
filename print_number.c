728719ec584a1ce950bb7d1d5e62ba61803560cc
sagikarni4 <sagikarni4@gmail.com>
2022-04-04 14:24:19 +0300

    Fixed printing issue in print_number.c

--- print_number.c
+++ print_number.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#define N 6
 
-int course_number[6] = {4, 2, 1, 4, 3, 2};
+int course_number[N] = {4, 2, 1, 4, 3, 2};
 int main() {
-	for(int i = 6; i > 0; i--) {
+	for(int i = N-1; i >= 0; i--) {
 		printf("%d", course_number[i]);
 	}
 	printf("\n");
